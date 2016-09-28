58d229f4994700defe230e6516dfc3a8644cb358
mousezero <rm@mousezero.com>
2016-09-28 16:43:49 -0700

    added compression

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict'
 const express = require('express')
+const compression = require('compression')
 const app = express()
 
+app.use(compression())
 app.get('/', (req, res, next) => {
   let ipArray = req.ip.split(':')
   const result = {
